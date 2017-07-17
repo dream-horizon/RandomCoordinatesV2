70f2bcf3f96c5de4042955a513ed4f339a56e963
PHENOMICAL <thephenom1811@gmail.com>
2017-07-17 05:33:39 +0200

    Should finally work

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -6,6 +6,6 @@ sudo apt-get -qq update
 sudo apt-get install -y jq
 curl -H "Authorization: token $GITPERM" https://api.github.com/users/PHENOMICAL -I
 VERSION_URL=$(curl -H "Content-Type: application/json" -X POST -d "{\"description\":\"Created through Travis-CI\",\"public\":\"false\",\"files\":{\"version.yml\":{\"content\":\"Branch: ${TRAVIS_BRANCH}\nBuild: ${TRAVIS_BUILD_NUMBER}\nVersion: ${TARGET_VERSION}\"}}}" https://api.github.com/gists | jq -r '.files."version.yml".raw_url')
-export VERSION_URL
 echo -e "Gist successfully deployed! URL: ${VERSION_URL}\n"
 echo -e "Finished deploying version info!\n"
+printf $VERSION_URL >> versionurl
