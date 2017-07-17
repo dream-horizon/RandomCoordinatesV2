f446cc76dc3e7f34340011d8d13b4360935c42b0
PHENOMICAL <thephenom1811@gmail.com>
2017-07-17 02:20:04 +0200

    Splitted travis.yml into scripts. Sick of YAML

---
+++ scripts/getDependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "Starting downloading dependencies\n"
+cd $TRAVIS_BUILD_DIR
+mkdir lib
+curl -o lib/GeneralLib.jar https://api.spiget.org/v2/resources/25348/download
+curl -o lib/Kingdoms.jar https://api.spiget.org/v2/resources/6392/download
+curl -o lib/Residence.jar http://ltcraft.lt/Residence/files/Residence4.7.0.5.jar
+curl -o lib/RedProtect.jar https://api.spiget.org/v2/resources/15841/download
+echo -e "Downloading of dependencies finished!\n"
---
+++ scripts/versioning.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "Deploying version info gist!\n"
+cd $TRAVIS_BUILD_DIR
+sudo apt-get -qq update
+sudo apt-get install -y jq
+curl -H "Authorization: token $GITPERM" https://api.github.com/users/PHENOMICAL -I
+export GIST_URL=$(curl --data '{"description":"Created through Travis-CI","public":"false","files":{"version.yml":{"content":"Branch: $TRAVIS_BRANCH\nBuild: $TRAVIS_BUILD_NUMBER\nVersion: $TARGET_VERSION"}}}' https://api.github.com/gists | js  '.files."version.yml".raw_url')
+echo -e "Gist successfully deployed! URL: $GIST_URL\n"
+echo -e "Finished deploying version info!\n"
